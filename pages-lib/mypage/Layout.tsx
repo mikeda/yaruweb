024022d9f3b50145ec29910ff58e2eef2967731b
mikeda <baguzy@gmail.com>
2021-04-22 08:14:59 +0900

    [WIP] moves/combos/videosをeditor権限のみ作成・更新可能にする

--- pages-lib/mypage/Layout.tsx
+++ pages-lib/mypage/Layout.tsx
@@ -9,15 +9,7 @@ interface Props {
 export const Layout: React.FC<Props> = ({ activeTab, children }) => {
   return (
     <>
-      <TabNav
-        tabs={[
-          { key: 'articles', href: Routes.mypageArticles(), label: '記事' },
-          { key: 'videos', href: Routes.mypageVideos(), label: 'オススメ動画' },
-          { key: 'combos', href: Routes.mypageCombos(), label: 'コンボ' },
-          { key: 'events', href: Routes.mypageEvents(), label: 'イベント' },
-        ]}
-        activeTabKey={activeTab}
-      />
+      <TabNav tabs={[{ key: 'articles', href: Routes.mypageArticles(), label: '記事' }]} activeTabKey={activeTab} />
 
       {children}
     </>
