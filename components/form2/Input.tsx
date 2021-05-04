29d22e470eed0dc65ac18010fa4d1a5de49f771f
mikeda <baguzy@gmail.com>
2021-05-04 10:24:59 +0900

    未使用ファイルを削除

--- components/form2/Input.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { UseFormRegisterReturn } from 'react-hook-form';
-
-export const Input = (props: Partial<UseFormRegisterReturn> & { type?: string }) => <input {...props} />;
