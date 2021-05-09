fc0c6cd0ac094cc2222eee837193f208354705da
mikeda <baguzy@gmail.com>
2021-05-09 22:10:14 +0900

    CommentForm周りのreact-hook-form対応

--- components/form2/TextArea.tsx
+++ components/form2/TextArea.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styles from './TextArea.module.scss';
 
-type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>;
+type Props = React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>;
 
 // eslint-disable-next-line react/display-name
 export const TextArea = React.forwardRef<HTMLTextAreaElement, Props>((props, ref) => (
