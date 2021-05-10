5f80631cfad49d6dfc54e5b37c201f0de9390ec9
mikeda <baguzy@gmail.com>
2021-05-10 21:39:53 +0900

    eslint

--- pages-lib/login/LoginWithEmailForm.tsx
+++ pages-lib/login/LoginWithEmailForm.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { Form, Formik } from 'formik';
-import * as Yup from 'yup';
 import { toast } from 'react-toastify';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
