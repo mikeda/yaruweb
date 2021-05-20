a4ea04af7cb2c02dc54c0317265d32327845965b
mikeda <baguzy@gmail.com>
2021-05-21 06:49:47 +0900

    コンボの動画が表示されていなかった

--- components/ComboMedia.tsx
+++ components/ComboMedia.tsx
@@ -2,21 +2,29 @@ import React from 'react';
 import { ComboFragment } from '@/lib/graphql/types';
 import { Operations } from './Command/Operations';
 
+import styles from './ComboMedia.module.scss';
+import { VideoPlayer } from './MoveMedia/VideoPlayer';
+
 interface Props {
   combo: ComboFragment;
 }
 
 export const ComboMedia: React.FC<Props> = ({ combo }) => {
   return (
-    <div className="bl_combo">
-      <div>{combo.name}</div>
-      <div className="bl_command hp_mg_b_md">
-        <div className="bl_command_inner">
-          <Operations operations={combo.operations} />
+    <div className={styles.container}>
+      <div className={styles.title}>{combo.name}</div>
+
+      {combo.comboVideo && (
+        <div className={styles.video}>
+          <VideoPlayer src={combo.comboVideo.m3u8Url} thumnailUrl={combo.comboVideo.thumbnailUrl} />
         </div>
+      )}
+
+      <div className={styles.command}>
+        <Operations operations={combo.operations} />
       </div>
 
-      {combo.note && combo.note.length > 0 && <div className="bl_move_note">{combo.note}</div>}
+      {combo.note && combo.note.length > 0 && <div className={styles.note}>{combo.note}</div>}
     </div>
   );
 };
