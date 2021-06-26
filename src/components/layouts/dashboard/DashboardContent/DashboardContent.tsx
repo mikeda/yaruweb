76e9190513b9fa98552abcf1fd1bb69c8a01249d
mikeda <baguzy@gmail.com>
2021-06-26 21:55:51 +0900

    ドロワーメニューのMaterial UI対応

--- src/components/layouts/dashboard/DashboardContent/DashboardContent.tsx
+++ src/components/layouts/dashboard/DashboardContent/DashboardContent.tsx
@@ -1,23 +1,118 @@
-import React from 'react';
-import { GlobalHeader } from '../../GlobalHeader';
+import React, { useState } from 'react';
 
-import styles from './DashboardContent.module.scss';
-import { Tabs, TabKey } from '../Tabs';
+import {
+  AppBar,
+  createStyles,
+  Drawer,
+  Hidden,
+  IconButton,
+  makeStyles,
+  Theme,
+  Toolbar,
+  Typography,
+  useTheme,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { DrawerItems } from './DrawerItems';
 
-interface Props {
-  activeTab: TabKey;
-}
+const drawerWidth = 240;
 
-export const DashboardContent: React.FC<Props> = ({ activeTab, children }) => {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      [theme.breakpoints.up('sm')]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      [theme.breakpoints.up('sm')]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+      },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up('sm')]: {
+        display: 'none',
+      },
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  }),
+);
+
+export const DashboardContent: React.FC = ({ children }) => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
   return (
-    <>
-      <GlobalHeader />
-
-      <div className={styles.container}>
-        <Tabs activeTab={activeTab} />
-
-        <div className={styles.content}>{children}</div>
-      </div>
-    </>
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Responsive drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            <DrawerItems />
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            <DrawerItems />
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <div>{children}</div>
+      </main>
+    </div>
   );
 };
