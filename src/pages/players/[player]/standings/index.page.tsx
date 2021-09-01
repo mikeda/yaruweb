d56cee3f16c98e5230443586abe43902a0b6e1ad
mikeda <baguzy@gmail.com>
2021-09-01 23:33:33 +0900

    Fix error

--- src/pages/players/[player]/standings/index.page.tsx
+++ src/pages/players/[player]/standings/index.page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 import { fetchGraphql } from '@/lib/graphql/fetchGraphql';
