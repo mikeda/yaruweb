16ef6f536ae08acd3ec2020483f2164cdafcc261
mikeda <baguzy@gmail.com>
2022-01-22 12:35:40 +0900

    NotFound/Loadingのstyled対応

---
+++ src/components/Loading.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { loadingState } from '@/states/loading';
+
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const Loading: React.FC = () => {
+  const loading = useRecoilValue(loadingState);
+  if (!loading) return null;
+
+  return (
+    <Container>
+      <Inner>
+        <div />
+        <div />
+        <div />
+      </Inner>
+    </Container>
+  );
+};
+
+const Container = styled('div')(() => ({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  zIndex: 10,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  height: '100%',
+  background: 'rgba(0, 0, 0, 0.3)',
+  transition: '0.3s linear',
+}));
+
+const scale = keyframes`
+  0%,
+  80% {
+    opacity: 1;
+    transform: scale(1);
+  }
+  45% {
+    opacity: 0.7;
+    transform: scale(0.1);
+  }
+`;
+
+const Inner = styled('div')`
+  & > div {
+    display: inline-block;
+    width: 15px;
+    height: 15px;
+    margin: 5px;
+    background-color: #fff;
+    border-radius: 100%;
+    animation-fill-mode: both;
+  }
+
+  & > div:nth-child(1) {
+    animation: ${scale} 0.75s -0.24s infinite cubic-bezier(0.2, 0.68, 0.18, 1.08);
+  }
+
+  & > div:nth-child(2) {
+    animation: ${scale} 0.75s -0.12s infinite cubic-bezier(0.2, 0.68, 0.18, 1.08);
+  }
+
+  & > div:nth-child(3) {
+    animation: ${scale} 0.75s 0s infinite cubic-bezier(0.2, 0.68, 0.18, 1.08);
+  }
+`;
