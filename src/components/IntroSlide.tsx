0a02f4d4009aedaea77b4b56ee9ad36b830dab52
Tomohiro Ikeda <baguzy@gmail.com>
2022-01-22 16:25:47 +0900

    IntroSlideのstyled対応 (#713)

---
+++ src/components/IntroSlide.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import { useMediaQuery, useTheme } from '@mui/material';
+
+interface Props {
+  imageUrl: string;
+}
+
+export const IntroSlide: React.FC<Props> = ({ imageUrl, children }) => {
+  const theme = useTheme();
+  const isPc = useMediaQuery(theme.breakpoints.up('sm'));
+
+  return (
+    <div>
+      <Inner isPc={isPc}>
+        <Image isPc={isPc}>
+          <img src={imageUrl} />
+        </Image>
+        <Content>{children}</Content>
+      </Inner>
+    </div>
+  );
+};
+
+interface ResponsiveProps {
+  isPc: boolean;
+}
+
+const Inner = styled('div')<ResponsiveProps>(({ isPc }) => ({
+  display: isPc ? 'flex' : 'block',
+  justifyContent: 'stretch',
+}));
+
+const Image = styled('div')<ResponsiveProps>(({ isPc }) => ({
+  width: isPc ? '50%' : '100%',
+  '& > img': {
+    width: '100%',
+  },
+}));
+
+const Content = styled('div')(() => ({
+  boxSizing: 'border-box',
+  display: 'flex',
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '96px',
+  padding: '16px',
+  lineHeight: 2,
+  textAlign: 'center',
+  border: '1px solid #ddd',
+}));
