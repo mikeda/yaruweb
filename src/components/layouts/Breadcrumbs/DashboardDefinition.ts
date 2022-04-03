9fdd0b1a440f8a1baed79156fa00cbba09de5808
mikeda <baguzy@gmail.com>
2022-04-03 10:45:59 +0900

    Fix error

--- src/components/layouts/Breadcrumbs/DashboardDefinition.ts
+++ src/components/layouts/Breadcrumbs/DashboardDefinition.ts
@@ -162,7 +162,7 @@ export const breadcrumbChain = (props: DashboardBreadcrumbParams): BreadcrumbCha
       };
     case 'tournamentVideo':
       return {
-        name: props.tournamentVideo.title,
+        name: props.tournamentVideo.label ? `${props.tournamentVideo.label}の対戦動画` : '対戦動画',
         parent: breadcrumbChain({ to: 'tournament', tournament: props.tournamentVideo.tournament }),
       };
     case 'tournamentVideoEdit':
