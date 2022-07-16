c7cbd207e81224d10ab27865f8b3de43ce351c40
mikeda <baguzy@gmail.com>
2022-07-16 21:34:51 +0900

    storybookのサンプルファイル削除

---
+++ src/components/ArticleCard.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ArticleCard } from '@/components';
+import { ArticleStatus } from '@/generated/graphql';
+
+export default {
+  title: 'Example/ArticleCard',
+  component: ArticleCard,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof ArticleCard>;
+
+const Template: ComponentStory<typeof ArticleCard> = args => <ArticleCard {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  article: {
+    id: 'id',
+    title: 'タイトル',
+    description: '本文',
+    status: ArticleStatus.Published,
+    mainImageUrl: 'https://d2ybk292wkc2jl.cloudfront.net/site/no_image.jpeg',
+    author: {
+      name: 'ミケダ',
+      avatarUrl: 'https://d2ybk292wkc2jl.cloudfront.net/site/no_image.jpeg',
+    },
+  },
+};
