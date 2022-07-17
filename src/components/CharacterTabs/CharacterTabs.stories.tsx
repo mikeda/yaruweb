b93d28aa88fc3b8796cf5c20f8833911b1fb3752
mikeda <baguzy@gmail.com>
2022-07-17 10:33:14 +0900

    コンポーネントごとにディレクトリを作る

---
+++ src/components/CharacterTabs/CharacterTabs.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { CharacterTabs } from '@/components';
+
+export default {
+  title: 'Example/CharacterTabs',
+  component: CharacterTabs,
+} as ComponentMeta<typeof CharacterTabs>;
+
+const Template: ComponentStory<typeof CharacterTabs> = args => <CharacterTabs {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  character: {
+    slug: 'josie',
+    battlesCount: 10,
+    combosCount: 20,
+    movesCount: 30,
+  },
+};
